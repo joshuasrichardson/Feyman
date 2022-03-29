939735128e379c49512a7003755ed108807715aa
Brady Richardson <bradyrr33@gmail.com>
2022-03-28 18:38:55 -0600

    add: mongo connection in app.js, started POST endpoint in index.js, created models folder and Article schema

--- back-end/app.js
+++ back-end/app.js
@@ -3,6 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose');
+
+// connect to the database
+mongoose.connect('mongodb://localhost:27017/feynman', {
+  useNewUrlParser: true
+});
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+//post the new article to the database
+router.post('/', (req, res, next) => {
+  
 });
 
 module.exports = router;
