e3a359436ad34f19f73183acc1a8eb8e472f0100
Joshua Richardson <joshuasrichardson1@gmail.com>
2022-03-28 18:49:32 -0600

    Add login view

---
+++ front-end/src/views/Login.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+	<h1>Login</h1>
+  <form class="login">
+    <label for="firstName">First Name</label>
+    <input type="text" name="firstName" value="">
+    <label for="lastName">Last Name</label>
+    <input type="text" name="lastName" value="">
+    <label for="firstName">First Name</label>
+    <input type="text" name="firstName" value="">
+    <label for="firstName">First Name</label>
+    <input type="text" name="firstName" value="">
+  </form>
+</div>
+</template>
+
+<script>
+export default {
+	name: "Login",
+};
+</script>
